58e4f759d7715378307e1a6f83b2a44d5177c300
Ram <ramtimilsina16@gmail.com>
2024-12-04 22:31:10 +0545

    form data for file upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+import FormData from "./FormData";
+function App() {
+  return (
+    <>
+      <header>
+        <FormData />
+      </header>
+    </>
+  );
+}
+
+export default App;
